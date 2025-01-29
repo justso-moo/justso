a9fec8ddf069ff751ed9de41603b9bb25485cbd2
justso-moo <hdab@duck.com>
2025-01-30 04:34:52 +0900

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,20 @@
 import sveltePreprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 export default {
   preprocess: sveltePreprocess()
 };
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
+};
+
+export default config;
